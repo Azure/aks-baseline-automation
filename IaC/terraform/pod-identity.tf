6ff2e7a5079a827d326c4118d13d9be17622cc25
Hieu Nguyen Nhu <5441003+hieumoscow@users.noreply.github.com>
2022-03-21 17:10:53 +0800

    Modified Terraform workflow 2

--- IaC/terraform/pod-identity.tf
+++ IaC/terraform/pod-identity.tf
@@ -1,11 +1,11 @@
-module "pod_identity_addon" {
-  source             = "./add-ons/aad-pod-identity"
-  aks_clusters       = module.caf.aks_clusters
-  aks_cluster_key    = var.aks_cluster_key
-  managed_identities = module.caf.managed_identities
-  aad_pod_identity   = var.aad_pod_identity
-}
+# module "pod_identity_addon" {
+#   source             = "./add-ons/aad-pod-identity"
+#   aks_clusters       = module.caf.aks_clusters
+#   aks_cluster_key    = var.aks_cluster_key
+#   managed_identities = module.caf.managed_identities
+#   aad_pod_identity   = var.aad_pod_identity
+# }
 
-output "pod_identity_addon_output" {
-  value = module.pod_identity_addon
-}
+# output "pod_identity_addon_output" {
+#   value = module.pod_identity_addon
+# }
