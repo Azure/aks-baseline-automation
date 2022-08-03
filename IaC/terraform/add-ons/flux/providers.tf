9cd65da55064e7388603914b1fa614c235b5ac90
Jose Luis Carrillo Aguilar <jose.carrillo@microsoft.com>
2022-08-03 21:04:11 +0200

    Providers re-ordered

--- IaC/terraform/add-ons/flux/providers.tf
+++ IaC/terraform/add-ons/flux/providers.tf
@@ -1,30 +1,6 @@
 provider "azurerm" {
-  partner_id = "451dc593-a3a3-4d41-91e7-3aadf93e1a78"
   features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
   }
-}
-
-provider "azurerm" {
-  alias                      = "vhub"
-  skip_provider_registration = true
-  features {}
-  subscription_id = data.azurerm_client_config.default.subscription_id
-  tenant_id       = data.azurerm_client_config.default.tenant_id
-}
-
-data "azurerm_client_config" "default" {}
-
-# Get kubeconfig from AKS clusters
-data "azurerm_kubernetes_cluster" "kubeconfig" {
-  name                = "akscluster-re1-001" #var.aks_clusters[var.aks_cluster_key].name
-  resource_group_name = "rg-bu0001a0008" #var.resource_groups[local.resourceGroupKey].name
-
-  depends_on = [
-    module.caf.aks_clusters
-  ]
 }
 
 provider "kubectl" {
@@ -46,6 +22,8 @@ provider "kubernetes" {
   cluster_ca_certificate = try(base64decode(data.azurerm_kubernetes_cluster.kubeconfig.kube_admin_config.0.cluster_ca_certificate), null)
 }
 
-provider "kustomization" {
-  kubeconfig_raw = data.azurerm_kubernetes_cluster.kubeconfig.kube_admin_config_raw
+# Get kubeconfig from AKS clusters
+data "azurerm_kubernetes_cluster" "kubeconfig" {
+  name                = var.aks_clusters[var.aks_cluster_key].cluster_name
+  resource_group_name = var.aks_clusters[var.aks_cluster_key].resource_group_name
 }
