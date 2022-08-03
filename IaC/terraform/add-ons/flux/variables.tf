6fe43e1f881a8f118f0f6eabfc10debb071a82e7
Jose Luis Carrillo Aguilar <jose.carrillo@microsoft.com>
2022-08-03 21:04:11 +0200

    Providers re-ordered

--- IaC/terraform/add-ons/flux/variables.tf
+++ IaC/terraform/add-ons/flux/variables.tf
@@ -1,1 +1,7 @@
+variable "aks_clusters" {
+  default = {}
+}
+
+variable "aks_cluster_key" {}
+
 variable "flux_settings" {}
--- IaC/terraform/main.tf
+++ IaC/terraform/main.tf
@@ -40,10 +40,6 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = ">= 1.11.1"
     }
-    kustomization = {
-      source  = "kbst/kustomization"
-      version = "~> 0.5.0"
-    }
     flux = {
       source  = "fluxcd/flux"
       version = ">= 0.0.14"
@@ -55,3 +51,23 @@ terraform {
   # comment it out for the local backend experience
   backend "azurerm" {}
 }
+
+
+provider "azurerm" {
+  partner_id = "451dc593-a3a3-4d41-91e7-3aadf93e1a78"
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+
+provider "azurerm" {
+  alias                      = "vhub"
+  skip_provider_registration = true
+  features {}
+  subscription_id = data.azurerm_client_config.default.subscription_id
+  tenant_id       = data.azurerm_client_config.default.tenant_id
+}
+
+data "azurerm_client_config" "default" {}
