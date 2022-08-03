605012421b3269c4ac229b1005b05c327b9bdbc4
Jose Luis Carrillo Aguilar <jose.carrillo@microsoft.com>
2022-08-03 12:28:39 +0200

    CAF Module source changed till 5.6.1 is released

--- IaC/terraform/module.tf
+++ IaC/terraform/module.tf
@@ -1,7 +1,7 @@
 module "caf" {
   #source  = "aztfmod/caf/azurerm"
   #version = "~> 5.6.1"
-  source = "github.com/aztfmod/terraform-azurerm-caf" # Referenced to main branch in the repo until issue #1280 is resolved on 5.6.1
+  source = "git::https://github.com/joselcaguilar/terraform-azurerm-caf?ref=bug/agw-private-subnet-cidr-optional" # Referenced to main branch in the repo until issue #1280 is resolved on 5.6.1
   # source = "./public"
 
   providers = {
