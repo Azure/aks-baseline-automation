cfe60e158c30e15c3b0603e6842f53b14ca1db4d
Isabelle Bersano <ibersano@microsoft.com>
2022-08-25 12:55:11 -0500

    Updated variable names and added inputs

--- IaC/scripts/createAppInfra.ps1
+++ IaC/scripts/createAppInfra.ps1
@@ -6,6 +6,7 @@
 param(
   [Parameter()]
   [String]$ResourceGroupName,
+  [String]$DefaultPrefix,
   [String]$AKSName,
   [String]$ACRName,
   [String]$Location
@@ -20,7 +21,8 @@ Write-Output "*** Check if Resource Group $ResourceGroupName exists"
 $checkRg = az group exists --name $ResourceGroupName | ConvertFrom-Json
 if (!$checkRg) {
   Write-Warning "*** WARN! Resource Group $ResourceGroupName does not exist. Creating..."
-  az group create --name $ResourceGroupName --location $Location
+  $name = "$DefaultPrefix-rg"
+  az group create --name $name --location $Location
 
   if ($LastExitCode -ne 0) {
     throw "*** Error - could not create resource group"
@@ -40,7 +42,7 @@ Write-Output "*** Check if ACR $ACRName exists"
 $checkAcr = az acr show --name $ACRName | ConvertFrom-Json
 if (!$checkAcr) {
   Write-Warning "*** WARN! ACR $ACRName does not exist. Creating..."
-  az acr create -n $ACRName -g $ResourceGroupName --location $Location --sku Standard --admin-enabled
+  az acr create -n $DefaultPrefix -g $ResourceGroupName --location $Location --sku Standard --admin-enabled
 
   if ($LastExitCode -ne 0) {
     throw "*** Error - could not create ACR"
@@ -60,7 +62,9 @@ Write-Output "*** Check if cluster $AKSName exists"
 $checkAKS = az aks show -n $AKSName -g $ResourceGroupName | ConvertFrom-Json
 if (!$checkAKS) {
   Write-Warning "*** WARN! AKS $AKSName does not exist. Creating..."
-  az aks create -g $ResourceGroupName -n $AKSName --enable-managed-identity --node-count 1 --enable-addons monitoring --enable-msi-auth-for-monitoring  --generate-ssh-keys --attach-acr $ACRName
+  
+  $name = "$DefaultPrefixaks"
+  az aks create -g $ResourceGroupName -n $name --enable-managed-identity --node-count 1 --enable-addons monitoring --enable-msi-auth-for-monitoring  --generate-ssh-keys --attach-acr $ACRName
 
   if ($LastExitCode -ne 0) {
     throw "*** Error - could not create cluster"
